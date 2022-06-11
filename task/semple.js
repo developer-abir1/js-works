1a17ed7b14d35191cb70dbd1c8d5d3f41f50b3c6
developer abir <developerabir40@gmail.com>
2022-06-11 20:49:14 +0600

    task

---
+++ task/semple.js
@@ -0,0 +1,36 @@
+//   1. conversation 
+
+function feetToInch(inc){
+
+    const feet  = inc / 12
+    return feet
+
+}
+
+const mamaFeet = feetToInch(300) 
+
+//  2. conversion 
+ function  centimeterToMeter(meter){
+
+  const centimeter = meter * 1000 
+  return centimeter
+ }
+
+ const fastMeter  = centimeterToMeter(7);
+ console.log(fastMeter)
+
+
+//  3. calcualtion
+// pageReuirements
+// book 1 hass 100 pages
+// book 2 hass 200 pages
+// book 3 hass 100 pages
+
+// 4. bestFriend 
+
+
+// bestFriend
+
+// 5.  onlyPositve will stop the loop any stop the number and retun positive number
+
+// onlyPositive
