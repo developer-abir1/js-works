e9cb119b423679a89e108546ab5c4fb2a40874d2
developer abir <developerabir40@gmail.com>
2022-06-17 11:40:32 +0600

    saveing ammount

--- task/assingmet/index.js
+++ task/assingmet/index.js
@@ -3,26 +3,49 @@ const mainAmount  = document.getElementById('incomeAmmount') ;
 const foodAmmmount  = document.getElementById('foodAmmmount') ;
 const rentAmmont  = document.getElementById('rentAmmont') ;
 const otherAmmoutn  = document.getElementById('otherAmmoutn') ;
+const totalExpenses = document.getElementById('totalExpenses');
+const blance = document.getElementById('blance');
+const saveAmmont = document.getElementById('saveAmmount');
+const saveingAmmount = document.getElementById('savingAmmon');
+const runningBalance = document.getElementById('runnigBalance');
+
 
 const otherBuy =[
     foodAmmmount,
     rentAmmont,
     otherAmmoutn
-]
+] 
+const handleCalculate =()=>{ 
+
+           let TotlaotherValue = 0;
+            for(const others of otherBuy){
+             
+                const othersNumber = others.value 
+                const othersValue = parseFloat(othersNumber)
+                TotlaotherValue = othersValue + TotlaotherValue;
+
+               others.value = "" 
+            }
+            const mainAmountNumber  = parseFloat(mainAmount.value)
+         
+    
+        const slaryAmmout = mainAmountNumber -   TotlaotherValue;
+        const totalExpensesNumber=  totalExpenses.innerText = TotlaotherValue;
+        const blanceAmount = blance.innerText = slaryAmmout
+        return totalExpensesNumber ,blanceAmount
+
+        }    
+        // handle Save Ammount 
+        const handleSaveAmmount =() =>{
+
+            const saveAmmountNumber = parseInt(saveAmmont.value);
+            const mainAmmount = parseInt(mainAmount.value);
+            const saveAmmounts = saveAmmountNumber * mainAmmount ;
+            const totalAmounts = saveAmmounts / 100;
+            const saveingAmmouns = saveingAmmount.innerText = totalAmounts; 
+const runningBalanceAmmount = mainAmmount -  saveingAmmouns;
+        const   totalRunningBalance  =   runningBalance.innerText = runningBalanceAmmount
+            return saveingAmmouns , totalRunningBalance
 
 
-const handleCalculate =()=>{
-    
-    if(foodAmmmount === Number && rentAmmont === Number && otherAmmoutn  === Number ){
-        
-for(const others of otherBuy){
-    const 
-    console.log(others.value)
-}
-    }
-    else{
-        alert("plase enter number ")
-    }
-    
-    console.log(mainAmount.value)
-}
+        }
